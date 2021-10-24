044c0f645df9ab72c8508cd9e9824a72e85a7ef5
Joseph Estevez <mateobackground@gmail.com>
2021-10-24 15:27:49 -0400

    initial commit

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,4 @@
+// Heaven's on Fire
+export default function handler(req, res) {
+  res.status(200).json({ name: 'OK' })
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <Image height={"300"} width={"300"} src="/gallery/1.jpg" alt="..." />
+    </div>
+  )
+}

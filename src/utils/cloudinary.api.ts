2d5d04a17839ab42350f94ac5a831d8286c9bf1e
Joseph Estevez <mateobackground@gmail.com>
2021-10-26 17:30:04 -0400

    Remove 'ConfigService'
    Next has its own system for loading environment variables

--- src/utils/cloudinary.api.ts
+++ src/utils/cloudinary.api.ts
@@ -1,13 +1,10 @@
 import cloudinary from 'cloudinary';
-import ConfigService from '../libs/configService';
-
-const config = new ConfigService();
 
 const storage = (): void => {
   cloudinary.v2.config({
-    cloud_name: config.get('CLOUDINARY_NAME'),
-    api_key: config.get('CLOUDINARY_API_KEY'),
-    api_secret: config.get('CLOUDINARY_API_SECRET'),
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true,
   });
 };

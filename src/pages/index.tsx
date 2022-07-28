6ccd13f327e318ebeddd5c546af8311861d61ab6
joseph Estevez <joseph.estevez@protonmail.com>
2022-07-28 17:19:49 -0400

    BREAKING CHANGE: remove data loader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import { GetStaticProps, NextPage } from 'next'
-import { useDispatch } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { ActionCreators } from '../common/states'
 import { fetchGallery } from '$mod/cloudinary/services/fetchData'
 import PropTypes from 'prop-types'
 import Gallery from '$layouts/gallery'
@@ -11,13 +8,20 @@ interface HomeProps {
   gallery: any
 }
 
+export const getStaticProps: GetStaticProps = async () => {
+  const resources = await fetchGallery({ type: 'upload', prefix: 'gallery', max_results: 50 })
+
+  return {
+    props: {
+      gallery: {
+        resources
+      }
+    },
+    revalidate: 10
+  }
+}
+
 const Home: NextPage<HomeProps> = ({ gallery }) => {
-  const dispath = useDispatch()
-  const { setGalleryItems } = bindActionCreators(ActionCreators, dispath)
-
-  setGalleryItems(gallery.data)
-  console.log(gallery)
-
   return (
     <Layout>
       <Gallery />
@@ -27,23 +31,6 @@ const Home: NextPage<HomeProps> = ({ gallery }) => {
 
 Home.propTypes = {
   gallery: PropTypes.any
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const galleryData = await fetchGallery({ type: 'upload', prefix: 'gallery', max_results: 50 })
-
-  return {
-    props: {
-      gallery: {
-        data: galleryData.resources.map((img: any) => ({
-          id: img.asset_id,
-          pictureSrc: img.secure_url,
-          alternativeText: img.secure_url.split('/').pop() ?? ''
-        }))
-      }
-    },
-    revalidate: 10
-  }
 }
 
 export default Home

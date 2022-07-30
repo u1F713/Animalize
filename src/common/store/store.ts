3c908489e1a2ed13a0ade301fb48bd0a13a31bde
joseph Estevez <joseph.estevez@protonmail.com>
2022-07-30 18:03:51 -0400

    feat: add store app selector

--- src/common/store/store.ts
+++ src/common/store/store.ts
@@ -3,3 +3,6 @@ import { reducer } from './reducers'
 import thunk from 'redux-thunk'
 
 export const store = createStore(reducer, {}, applyMiddleware(thunk))
+
+export type RootState = ReturnType<typeof reducer>
+export type AppDispatch = typeof store.dispatch

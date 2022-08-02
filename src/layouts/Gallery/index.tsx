d64c09a6f0ac0e9cee53dda85ed7b3f9ab8eabc1
joseph Estevez <joseph.estevez@protonmail.com>
2022-08-01 20:33:00 -0400

    fix: imports

--- src/layouts/Gallery/index.tsx
+++ src/layouts/Gallery/index.tsx
@@ -1,21 +1,3 @@
-import { memo, ReactElement, useEffect } from 'react'
-import { useAppDispatch } from '$common/store'
-import { addTodo, GalleryAdapter } from '$mod/gallery'
-import { isEqual } from 'lodash'
-import GalleryComponent from './Gallery'
+import Gallery from './Adapter'
 
-interface GalleryProps {
-  resources: GalleryAdapter
-}
-
-function GalleryCamponentAdapted({ resources }: GalleryProps): ReactElement {
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(addTodo(resources))
-  }, [resources])
-
-  return <GalleryComponent />
-}
-
-export const Gallery = memo(GalleryCamponentAdapted, isEqual)
+export default Gallery

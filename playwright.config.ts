a911bfb1e86a58d4f2781f8c91684f6403028b17
Joseph Estevez <joseph.estevez@protonmail.com>
2022-08-18 20:06:11 -0400

    feat: update playwright config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,29 +1,31 @@
 import type { PlaywrightTestConfig } from '@playwright/test'
 import { devices } from '@playwright/test'
-import { config } from 'dotenv'
-
-config({ path: '.env.local' })
 
 const playwrightConfiguration: PlaywrightTestConfig = {
   testDir: './',
   reporter: 'html',
   outputDir: 'tests/results/',
   testIgnore: '**/node_module/**',
+  fullyParallel: true,
+  forbidOnly: !(process.env.CI == null),
+  retries: process.env.CI != null ? 2 : 0,
+  workers: process.env.CI != null ? 1 : undefined,
 
   timeout: 30 * 1000,
   expect: {
     timeout: 5000
   },
 
-  fullyParallel: true,
-  forbidOnly: !(process.env.CI == null),
-  retries: process.env.CI != null ? 2 : 0,
-  workers: process.env.CI != null ? 1 : undefined,
-
   use: {
     actionTimeout: 0,
     baseURL: 'http://localhost:1616',
     trace: 'on-first-retry'
+  },
+
+  webServer: {
+    command: 'pnpm next dev --port 1616',
+    port: 1616,
+    reuseExistingServer: process.env.CI == null
   },
 
   projects: [
@@ -40,12 +42,7 @@ const playwrightConfiguration: PlaywrightTestConfig = {
         ...devices['Desktop Chrome']
       }
     }
-  ],
-
-  webServer: {
-    command: 'pnpm next dev --port 1616',
-    port: 1616
-  }
+  ]
 }
 
 export default playwrightConfiguration
